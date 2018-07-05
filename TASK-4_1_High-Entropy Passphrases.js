daf284768ebaf8f4e6a48d78b9b91df71dfb94e2
Ramu M <ramlaksh68@gmail.com>
2018-07-05 16:21:52 +0530

    Task_1 Day 4

--- TASK-4_1_High-Entropy Passphrases.js
+++ TASK-4_1_High-Entropy Passphrases.js
@@ -1,1 +1,17 @@
-console.log(1)
+var text=`kvvfl kvvfl olud wjqsqa olud frc
+slhm rdfm yxb rsobyt rdfm
+cgc jazrp crgnna uvuokl uvuokl uoiwl sknmc sknmc
+rvbu czwpdit vmlihg spz lfaxxev zslfuto oog dvoksub`;
+isHaveDubWord(text);
+function isHaveDubWord(text){
+
+   let grp=text.split(/\n/);
+   var result= grp.reduce((sum,sntnce)=>{
+      var wrdsAry=sntnce.split(/\s/);
+      var afterdubrmv=[...new Set(wrdsAry)];
+      sum+=(afterdubrmv.length==wrdsAry.length)?1:0;
+     return sum;
+   },0);
+   
+     console.log(result)
+ }
